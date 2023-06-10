375c28acb6d16faf583c133cd0c0867d6983993c
chinekwe01 <chinekwe_clever@outlook.com>
2023-06-10 11:24:48 +0100

    Delete product from cart

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/shop', [ProductController::class, 'index'])->name('shop');
 Route::get('/shop/{id}', [ProductController::class, 'show'])->name('products');
 Route::get('/cart', [CartController::class, 'cart'])->name('cart');
 Route::get('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/delete-from-cart/{id}', [CartController::class, 'delete'])->name('delete.from.cart');
